869ac1ed6fb0e8eb836d6fd0d5b2cb9314e4a50e
Timani G <timanig@gmail.com>
2019-11-06 10:55:26 -0500

    /Method Calling
    * Methods *
    -------------
    **Summary**
     Practicing calling methods

---
+++ Methods/Methods/Methods.cs
@@ -0,0 +1,33 @@
+﻿using System;
+namespace Methods
+{
+    public class Methods
+    {
+        public Methods()
+        {
+            Console.WriteLine("Welcome to the party");
+            Normal(3);
+            Named(3);
+            Optional(5);
+            Optional(5, "loo");
+
+            // THIS FUNCTION IS BEING USED WITH OPTIONAL AND NAMED
+            Optional(b: "loo", a: 3);
+        }
+        //Normal Function
+        void Normal(int a)
+        {
+            Console.WriteLine(a);
+        }
+        //Named Paramaters
+        void Named(float a)
+        {
+            Console.WriteLine((int)a);
+        }
+        //Optional Parameters
+        void Optional(int a, string b = "woo")
+        {
+            Console.WriteLine(a + b);
+        }
+    }
+}
